fa4e1e1922075aa034c7936d5742eeb4f0b36005
Khang <khang.lenguyen192@gmail.com>
2022-12-06 09:58:43 +0700

    add migration db

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,4 +1,15 @@
+using Backend.DBContext;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.UseKestrel()
+    .ConfigureAppConfiguration((hostingContext, config) =>
+    {
+        config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+        config.AddEnvironmentVariables();
+    });
 
 // Add services to the container.
 
@@ -6,6 +17,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<BackendSystemContext>(options =>
+{
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+                     new MySqlServerVersion(new Version(8, 0, 27)));
+});
+builder.Services.AddScoped<DbContext>(serviceProvider => serviceProvider.GetRequiredService<BackendSystemContext>());
+builder.WebHost.UseIIS();
+builder.WebHost.UseIISIntegration();
 
 var app = builder.Build();
 
