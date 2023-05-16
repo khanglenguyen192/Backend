10aebd9555a4f5f39d4b2765767bad3caf776295
Khang <khang.lenguyen192@gmail.com>
2023-05-16 18:14:11 +0700

    fix bug edit report

--- Backend.Common/Models/Report/ReportResponseModel.cs
+++ Backend.Common/Models/Report/ReportResponseModel.cs
@@ -14,6 +14,7 @@ namespace Backend.Common
         public int TicketId { get; set; }
         public string? Title { get; set; }
         public string? Content { get; set; }
+        public int UserId { get; set; }
         public DateTime Created { get; set; }
 
         public DateTime Modified { get; set; }
